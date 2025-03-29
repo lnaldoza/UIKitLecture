52a48927e95fda5a5b6b7988686ed17f5c63a29d
naldoza <itc.naldoza@gmail.com>
2025-03-29 12:22:55 +0800

    UIKitLecture project folder

---
+++ UIKitLectures/UIKitLecturesTests/UIKitLecturesTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitLecturesTests.swift
+//  UIKitLecturesTests
+//
+//  Created by Leinard Naldoza on 3/29/25.
+//
+
+import Testing
+@testable import UIKitLectures
+
+struct UIKitLecturesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
